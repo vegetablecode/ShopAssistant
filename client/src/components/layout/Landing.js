aee754696dcbf118f961088fe79e1dfba9a24112
Kamil <kamil4525@gmail.com>
2019-06-29 19:46:26 +0200

    add simple landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,6 +3,18 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 
+import {
+  Jumbotron,
+  Button,
+  Card,
+  CardText,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Row,
+  Col
+} from "reactstrap";
+
 class Landing extends Component {
   static propTypes = {
     isAuthenticated: PropTypes.bool
@@ -14,8 +26,77 @@ class Landing extends Component {
     }
   }
 
+  learnMoreAction = () => {
+    this.props.history.push("/about");
+  };
+
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">Helo Łorld!</h1>
+          <p className="lead">A komu to po co?</p>
+          <hr className="my-2" />
+          <p>Appka do robienia listy zakupów meh.</p>
+          <p className="lead">
+            <Button color="primary" onClick={this.learnMoreAction}>
+              Learn More
+            </Button>
+          </p>
+        </Jumbotron>
+        <h1 className="my-4">Welcome to Appka</h1>
+        <Row>
+          <Col className="lg-4 mb-4">
+            <Card h="100">
+              <CardHeader>
+                <h4>Cart Tytul</h4>
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Sapiente esse necessitatibus neque.
+                </CardText>
+              </CardBody>
+              <CardFooter>
+                <Button>Learn More</Button>
+              </CardFooter>
+            </Card>
+          </Col>
+          <Col lg="4" mb="4">
+            <Card h="100">
+              <CardHeader>
+                <h4>Cart Tytul</h4>
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Sapiente esse necessitatibus neque.
+                </CardText>
+              </CardBody>
+              <CardFooter>
+                <Button>Learn More</Button>
+              </CardFooter>
+            </Card>
+          </Col>
+          <Col lg="4" mb="4">
+            <Card h="100">
+              <CardHeader>
+                <h4>Cart Tytul</h4>
+              </CardHeader>
+              <CardBody>
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Sapiente esse necessitatibus neque.
+                </CardText>
+              </CardBody>
+              <CardFooter>
+                <Button>Learn More</Button>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
