19d012c594c7484cb220fbba92f8600d00103041
Kamil <kamil4525@gmail.com>
2019-07-01 21:09:19 +0200

    test on heroku 2

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const config = require("config");
 
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Bodyparser Middleware
 app.use(express.json());
 
@@ -45,16 +48,13 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
+app.listen(port);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+console.log(`App listening on ${port}`);
